0a23945b027bfd9bcfee2bbc1fa3c22efa4b44b8
Morgane Dubus <morgane.d@meilisearch.com>
2021-10-28 14:10:08 +0200

    Add instant MeiliSearch

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import { appWithTranslation } from 'next-i18next'
+import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from 'theme/GlobalStyle'
 import theme from 'theme'
