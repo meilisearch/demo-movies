6fe4f4476f5be8bd560c33ef618a8652fcf4242b
Morgane Dubus <morgane.d@meilisearch.com>
2021-11-02 08:55:48 +0100

    Add GlobalStyle

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,15 @@
 import { appWithTranslation } from 'next-i18next'
+import GlobalStyle from 'theme/GlobalStyle'
 import 'theme/colors.css'
 
 // Template for every page
 export function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default appWithTranslation(App)
