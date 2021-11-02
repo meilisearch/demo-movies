38d93a4ba5343cf4d5355499366a57f9fd900f6e
Morgane Dubus <morgane.d@meilisearch.com>
2021-11-02 14:33:08 +0100

    Add fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -30,7 +30,22 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Asap+Condensed:wght@600&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Barlow:wght@300;500;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body className="dark">
           <script
             dangerouslySetInnerHTML={{
