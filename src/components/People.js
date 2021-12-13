9e2e615b62767c9123b44ec93455d688fe7b985c
Morgane Dubus <morgane.d@meilisearch.com>
2021-12-13 11:53:17 +0100

    Add placeholder for people without images

--- src/components/People.js
+++ src/components/People.js
@@ -36,17 +36,31 @@ const CharacterName = styled(Typography)`
   }
 `
 
+const PlaceholderProfilePicture = styled.div`
+  width: 100%;
+  position: relative;
+  height: 0;
+  padding-bottom: 100%;
+  border-radius: 50%;
+  background-color: var(--people-placeholder);
+`
+
 const People = ({ people, ...props }) => {
   const { character = '', name = '', profile_path = '' } = people
   return (
     <Wrapper {...props}>
-      <ProfilePicture
-        src={`https://image.tmdb.org/t/p/w185/${profile_path}`}
-        alt={name}
-        layout="responsive"
-        width={100}
-        height={100}
-      />
+      {profile_path ? (
+        <ProfilePicture
+          src={`https://image.tmdb.org/t/p/w185/${profile_path}`}
+          alt={name}
+          layout="responsive"
+          width={100}
+          height={100}
+        />
+      ) : (
+        <PlaceholderProfilePicture />
+      )}
+
       <Text>
         <PeopleName variant="typo4">{name}</PeopleName>
         <CharacterName variant="typo4">{character}</CharacterName>
