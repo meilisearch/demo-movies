776027e4a3a65573ba1867f767373cd1baf2f988
Morgane Dubus <morgane.d@meilisearch.com>
2022-06-08 10:54:55 +0200

    Update cypress config to remove import of unexisting plugins

--- cypress.config.js
+++ cypress.config.js
@@ -10,9 +10,9 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
+    // setupNodeEvents(on, config) {
+    //   return require('./cypress/plugins/index.js')(on, config)
+    // },
     baseUrl: 'http://localhost:3000',
   },
 })
