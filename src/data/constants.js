c7a6a15679a33c7b83226dc43a813dceb347ac9f
shivaylamba <shivaylamba@gmail.com>
2022-07-23 22:56:59 +0530

    Define supported languages

---
+++ src/data/constants.js
@@ -0,0 +1,18 @@
+export const LANGUAGES = {
+  English: {
+    code: 'en-US',
+    indexName: 'movies-en-US',
+  },
+  Japanese: {
+    code: 'ja-JP',
+    indexName: 'movies-ja-JP',
+  },
+  Mandarin: {
+    code: 'zh-CN',
+    indexName: 'movies-zh-CN',
+  },
+  Hebrew: {
+    code: 'he-IL',
+    indexName: 'movies-he-IL',
+  },
+}
