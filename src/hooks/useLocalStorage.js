f7ab38b4e9a85f9d360cbf42be201c32c1050534
Morgane Dubus <morgane.d@meilisearch.com>
2022-09-06 07:44:33 +0200

    Add safe checks inside the uselocalstorage custom hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 
 function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
+    if (typeof window === 'undefined') return initialValue
     try {
       const item = window.localStorage.getItem(key)
       return item ? JSON.parse(item) : initialValue
@@ -13,13 +14,15 @@ function useLocalStorage(key, initialValue) {
   })
 
   const setValue = value => {
-    try {
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value
-      setStoredValue(valueToStore)
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
-    } catch (error) {
-      console.log(error)
+    if (typeof window !== 'undefined') {
+      try {
+        const valueToStore =
+          value instanceof Function ? value(storedValue) : value
+        setStoredValue(valueToStore)
+        window.localStorage.setItem(key, JSON.stringify(valueToStore))
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 
