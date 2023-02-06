b8447db4d6a57579b4d3a0c17962fc9e07b99ed6
Strift <laurent@meilisearch.com>
2023-02-06 14:08:40 +0100

    Add fathom analytics in production

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,6 +4,7 @@ import GlobalStyle from 'theme/GlobalStyle'
 import theme from 'theme'
 import 'theme/colors.css'
 import get from 'utils/get'
+import Script from 'next/script'
 
 const Wrapper = styled.div`
   @media (max-width: ${get('breakpoints.desktop')}) {
@@ -11,11 +12,27 @@ const Wrapper = styled.div`
   }
 `
 
+const Scripts = () => {
+  if (process.env.NEXT_PUBLIC_APP_ENV !== 'production') {
+    return null
+  }
+  return (
+    <>
+      <Script
+        id="fathom-script"
+        src="https://cdn.usefathom.com/script.js"
+        data-site="VNCRAVTB"
+      />
+    </>
+  )
+}
+
 // Template for every page
 export function App({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Scripts />
       <Wrapper>
         <Component {...pageProps} />
       </Wrapper>
