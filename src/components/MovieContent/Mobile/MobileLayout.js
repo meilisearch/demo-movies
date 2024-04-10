ae3843ec4c95430c447f0cd4884f0502aaed0292
Bruno Casali <brunoocasali@gmail.com>
2024-04-10 18:09:09 +0200

    Fix yarn lint

--- src/components/MovieContent/Mobile/MobileLayout.js
+++ src/components/MovieContent/Mobile/MobileLayout.js
@@ -9,7 +9,6 @@ import Cast from '../Cast'
 import BackdropImage from '../BackdropImage'
 import Poster from 'components/Poster'
 import { useTranslation } from 'next-i18next'
-import Favorite from '../../Favorite'
 
 const Wrapper = styled.div`
   height: 100%;
