ee0b73db7a530e8747ebef017b43d6ae18135475
Strift <laurent@meilisearch.com>
2024-08-15 12:46:35 +0200

    update settings script

--- scripts/update-settings.ts
+++ scripts/update-settings.ts
@@ -18,15 +18,15 @@ const DESCRIPTION_MAXIMUM_WORDS = 100
 const indexesConfig = [
   {
     indexName: 'movies-en-US',
-    documentTemplate: `A movie titled '{{doc.title}}', released in {{ doc.release_date | date: '%B %d, %Y' }}, whose description starts with {{doc.overview|truncatewords: ${DESCRIPTION_MAXIMUM_WORDS}}}`,
+    documentTemplate: `A movie titled '{{doc.title}}' that released in {{ doc.release_date | date: '%Y' }}. The movie genres are: {{doc.genres}}. The storyline is about: {{doc.overview|truncatewords: ${DESCRIPTION_MAXIMUM_WORDS}}}`,
   },
   {
     indexName: 'movies-ja-JP',
-    documentTemplate: `映画のタイトルは '{{doc.title}}' で、{{ doc.release_date | date: '%Y年%m月%d日' }}に公開され、{{doc.overview|truncatewords: ${DESCRIPTION_MAXIMUM_WORDS}}}`,
+    documentTemplate: `映画のタイトルは '{{doc.title}}' で、{{ doc.release_date | date: '%Y年' }}に公開され、ジャンルは: {{doc.genres}}。あらすじは: {{doc.overview|truncatewords: ${DESCRIPTION_MAXIMUM_WORDS}}}`,
   },
   {
     indexName: 'movies-th-TH',
-    documentTemplate: `หนังชื่อ '{{doc.title}}' ฉายเมื่อ {{ doc.release_date | date: '%d %B %Y' }} ซึ่งเริ่มต้นด้วย {{doc.overview|truncatewords: ${DESCRIPTION_MAXIMUM_WORDS}}}`,
+    documentTemplate: `หนังชื่อ '{{doc.title}}' ฉายเมื่อ {{ doc.release_date | date: '%Y' }} ซึ่งเริ่มต้นด้วย {{doc.genres}}. เรื่องย่อเกี่ยวกับ: {{doc.overview|truncatewords: ${DESCRIPTION_MAXIMUM_WORDS}}}`,
   },
 ]
 
@@ -34,6 +34,7 @@ async function main() {
   console.log(`Connecting to: ${MEILISEARCH_HOST}`)
 
   const shouldEnableVectorStore = !isUsingMeilisearchCloud(MEILISEARCH_HOST)
+  // On Meilisearch Cloud, feature needs to be enabled manually
   if (shouldEnableVectorStore) {
     console.log('Enabling vector store')
     await ofetch<TaskResponse>(`${MEILISEARCH_HOST}/experimental-features`, {
@@ -50,34 +51,38 @@ async function main() {
   for (const { indexName, documentTemplate } of indexesConfig) {
     const endpoint = `${MEILISEARCH_HOST}/indexes/${indexName}/settings`
 
-    const task = await ofetch<TaskResponse>(endpoint, {
-      method: 'PATCH',
-      headers: {
-        Authorization: `Bearer ${MEILISEARCH_ADMIN_API_KEY}`,
-      },
-      body: {
-        embedders: {
-          default: {
-            source: 'openAi',
-            apiKey: OPENAI_API_KEY,
-            model: 'text-embedding-ada-002',
-            documentTemplate: documentTemplate,
-          },
-          small: {
-            source: 'openAi',
-            apiKey: OPENAI_API_KEY,
-            dimensions: 1024,
-            model: 'text-embedding-3-small',
-            documentTemplate: documentTemplate,
+    try {
+      const task = await ofetch<TaskResponse>(endpoint, {
+        method: 'PATCH',
+        headers: {
+          Authorization: `Bearer ${MEILISEARCH_ADMIN_API_KEY}`,
+        },
+        body: {
+          embedders: {
+            small: {
+              source: 'openAi',
+              apiKey: OPENAI_API_KEY,
+              dimensions: 1024,
+              model: 'text-embedding-3-small',
+              documentTemplate: documentTemplate,
+            },
+            text_large: {
+              source: 'openAi',
+              apiKey: OPENAI_API_KEY,
+              dimensions: 3072,
+              model: 'text-embedding-3-large',
+              documentTemplate: documentTemplate,
+            },
           },
         },
-      },
-    })
-
-    console.log('Enqueued task: ', task)
+      })
+      console.log('Enqueued task: ', task)
+    } catch (error) {
+      console.error('Error: ', JSON.stringify(error))
+    }
   }
   console.log(
-    'All tasks enqueued successfully! Check tasks status via: https://cloud.meilisearch.com/'
+    `All tasks enqueued! Check tasks status via: https://cloud.meilisearch.com/`
   )
 }
 
