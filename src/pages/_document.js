7ce89e7092bf6d867eabb9199da6c5767cae5a42
CaroFG <carolina.ferreira131@gmail.com>
2025-06-23 20:58:46 +0200

    Add GTM

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,6 +31,17 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* Google Tag Manager */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TNSCGVBH');`,
+            }}
+          />
+          {/* End Google Tag Manager */}
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
@@ -64,6 +75,16 @@ class MyDocument extends Document {
           />
         </Head>
         <body className="dark">
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe 
+              src="https://www.googletagmanager.com/ns.html?id=GTM-TNSCGVBH"
+              height="0" 
+              width="0" 
+              style={{display:'none',visibility:'hidden'}}
+            />
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
           <script
             dangerouslySetInnerHTML={{
               __html: `
